f7b0649d9c3c35e87f9e02a5a36984fe286fb22d
enondorf934 <enondorf934@gmail.com>
2018-02-15 19:15:34 -0600

    Finished CreateOutdoors
    Finished CreateOutdoors including the api part

--- src/api/outdoors-api.js
+++ src/api/outdoors-api.js
@@ -3,5 +3,12 @@ import axios from 'axios';
 import { API_BASE } from './index';
 import { formatData } from './utils';
 
-export const createOutdoor = (fields) => {	
+export const createOutdoor = async (fields) => {	
+    try {
+		const outdoor = formatData(fields);
+		const response = await axios.post(`${API_BASE}/outdoor`, { ...outdoor });
+		return response;
+	} catch (e) {
+		return e;
+	}
 };
--- src/outdoors/forms/CreateOutdoors.jsx
+++ src/outdoors/forms/CreateOutdoors.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { BaseForm } from '../../forms';
+import { createOutdoor } from '../../api';
 import { LabeledSelect, LabeledField } from '../../forms/common';
 
 const TYPE_OF_TRAILS = [
@@ -16,15 +17,36 @@ const DIFFICULTY_OF_TRAIL = [
 ];
 
 class CreateOutdoors extends Component {
-	state = {}
+	state = {
+		trailType: '',
+		trailDifficulty: '',
+		trailDistance: 0,
+
+	}
+
+	onChange = (name, value) => {
+		this.setState({
+			[name]: value,
+		});
+	}
+
+	onSubmit = (fields) => {
+		const { trailType, trailDifficulty, trailDistance } = this.state;
+		createOutdoor({ trailType, trailDifficulty, trailDistance, ...fields });
+	}
+
 	render() {
+		const { trailType, trailDifficulty, trailDistance } = this.state;
 		return (
-			<div>
+			<section className="outdoors-form">
 				<h3 className="form-header-title">Create an Outdoor item</h3>
-				<LabeledSelect name="trailType" label="Type of trail:" options={TYPE_OF_TRAILS} />
-				<LabeledSelect name="trailDifficulty" label="Trail Difficulty:" options={DIFFICULTY_OF_TRAIL} />
-				<LabeledField name="trailDistance" label="Trail Distance:" placeholder="Enter number of KMs" />
-			</div>
+				<BaseForm onSubmit={this.onSubmit}>
+					<LabeledSelect name="trailType" label="Type of trail:" value={trailType} onChange={this.onChange} options={TYPE_OF_TRAILS} />
+					<br />
+					<LabeledSelect name="trailDifficulty" label="Trail Difficulty:" value={trailDifficulty} onChange={this.onChange} options={DIFFICULTY_OF_TRAIL} />
+					<LabeledField name="trailDistance" label="Trail Distance:" value={trailDistance} onChange={this.onChange} placeholder="Enter number of KMs" />
+				</BaseForm>
+			</section>
 		);
 	}
 }
